38ff76e3f3a7dc9bbfde4119339a3bc63160e7e0
afelix <austinjfelix@gmail.com>
2021-04-10 19:11:08 -0400

    updated 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ const NotFoundPage: React.FC = () => (
   <Layout>
     <div>
       <h1>NOT FOUND</h1>
-      <p>You just hit a route that does not exist... the sadness.</p>
+      <p>You just hit a route that does not exist... Oh! the sadness.</p>
     </div>
   </Layout>
 );
