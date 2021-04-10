81ae8b1e3b2dc60e29670148a43601a4b502e8d0
afelix <austinjfelix@gmail.com>
2021-04-10 01:50:55 -0400

    Updated Navbar with mat-ui, still needs updating

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,29 @@
 import React, { useState } from 'react';
 import { Link } from 'gatsby';
-// import github from '../img/github-icon.svg';
-// import logo from '../img/logo.svg';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appbar: {
+      background: 'transparent',
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
 
 const Navbar = () => {
+  const classes = useStyles();
   const [state, setState] = useState({
     active: false,
     navbarActiveClass: '',
@@ -18,61 +38,44 @@ const Navbar = () => {
   };
 
   return (
-    <nav
-      className="navbar is-transparent"
-      role="navigation"
-      aria-label="main-navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item" title="Logo">
-            {/* <img src={logo} alt="Spyre" style={{ width: '88px' }} /> */}
-            <h4>Spyre</h4>
-          </Link>
-          {/* Hamburger menu */}
-          <div
-            className={`navbar-burger burger ${state.navbarActiveClass}`}
-            data-target="navMenu"
+    <AppBar position="static" className={classes.appbar} elevation={0}>
+      <Toolbar>
+        <Link to="/" className="navbar-item" title="Logo">
+          <Typography variant="h6" className={classes.title}>
+            Spyre
+          </Typography>
+        </Link>
+        <Link className="navbar-item" to="/about">
+          About
+        </Link>
+        <Link className="navbar-item" to="/products">
+          Products
+        </Link>
+        <Link className="navbar-item" to="/blog">
+          Blog
+        </Link>
+        <Link className="navbar-item" to="/contact">
+          Contact
+        </Link>
+        <Link className="navbar-item" to="/contact/examples">
+          Form Examples
+        </Link>
+        <div
+          className={`navbar-burger burger ${state.navbarActiveClass}`}
+          data-target="navMenu"
+        >
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
             onClick={() => toggleHamburger()}
           >
-            <span />
-            <span />
-            <span />
-          </div>
+            <MenuIcon />
+          </IconButton>
         </div>
-        <div id="navMenu" className={`navbar-menu ${state.navbarActiveClass}`}>
-          <div className="navbar-start has-text-centered">
-            <Link className="navbar-item" to="/about">
-              About
-            </Link>
-            <Link className="navbar-item" to="/products">
-              Products
-            </Link>
-            <Link className="navbar-item" to="/blog">
-              Blog
-            </Link>
-            <Link className="navbar-item" to="/contact">
-              Contact
-            </Link>
-            <Link className="navbar-item" to="/contact/examples">
-              Form Examples
-            </Link>
-          </div>
-          {/* <div className="navbar-end has-text-centered">
-            <a
-              className="navbar-item"
-              href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon">
-                <img src={github} alt="Github" />
-              </span>
-            </a>
-          </div> */}
-        </div>
-      </div>
-    </nav>
+      </Toolbar>
+    </AppBar>
   );
 };
 
